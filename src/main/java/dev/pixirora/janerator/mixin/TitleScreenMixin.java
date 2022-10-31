a874077694037b1cc93a68478e32e706b31c20c5
Pixirora <rina@pixirora.dev>
2022-10-31 19:17:16 -0400

    Update metadata

--- src/main/java/dev/pixirora/janerator/mixin/TitleScreenMixin.java
+++ src/main/java/dev/pixirora/janerator/mixin/TitleScreenMixin.java
@@ -1,6 +1,6 @@
-package com.example.example_mod.mixin;
+package dev.pixirora.janerator.mixin;
 
-import com.example.example_mod.ExampleMod;
+import dev.pixirora.janerator.Janerator;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void exampleMod$onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		Janerator.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }

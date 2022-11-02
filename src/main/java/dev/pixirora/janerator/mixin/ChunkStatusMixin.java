b18b8a661bbf1fc84dd47caec92699376eff5e08
Pixirora <rina@pixirora.dev>
2022-11-02 19:36:39 -0400

    Make generation override with flat world for X<0
    Co-authored-by: LostLuma <lilly@lostluma.dev>

--- src/main/java/dev/pixirora/janerator/mixin/ChunkStatusMixin.java
+++ src/main/java/dev/pixirora/janerator/mixin/ChunkStatusMixin.java
@@ -3,7 +3,9 @@ package dev.pixirora.janerator.mixin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.minecraft.world.level.chunk.ChunkStatus;
+import net.minecraft.world.level.dimension.LevelStem;
 import net.minecraft.world.level.levelgen.presets.WorldPreset;
+import net.minecraft.world.level.levelgen.presets.WorldPresets;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArgs;
 // import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
-
+import net.minecraft.data.BuiltinRegistries;
 // import net.minecraft.server.level.ServerLevel;
 // import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 // import net.minecraft.server.level.ThreadedLevelLightEngine;
@@ -22,22 +24,28 @@ import net.minecraft.world.level.chunk.ChunkAccess;
 // import net.minecraft.server.level.ChunkHolder;
 // import java.util.concurrent.CompletableFuture;
 // import java.util.List;
+import net.minecraft.world.level.chunk.ChunkGenerator;
 
 @Mixin(ChunkStatus.class)
 public class ChunkStatusMixin {
     private static final Logger logger = LoggerFactory.getLogger("ChunkStatusMixin");
+
 	@ModifyArgs(
-        method = "(Ljava/util/concurrent/Executor;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplateManager;Lnet/minecraft/server/level/ThreadedLevelLightEngine;Ljava/util/function/Function;Ljava/util/List;Z)Ljava/util/concurrent/CompletableFuture;", 
+        method = "generate(Ljava/util/concurrent/Executor;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplateManager;Lnet/minecraft/server/level/ThreadedLevelLightEngine;Ljava/util/function/Function;Ljava/util/List;Z)Ljava/util/concurrent/CompletableFuture;", 
         at = @At(
             value="INVOKE",
             target="Lnet/minecraft/world/level/chunk/ChunkStatus$GenerationTask;doWork(Lnet/minecraft/world/level/chunk/ChunkStatus;Ljava/util/concurrent/Executor;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplateManager;Lnet/minecraft/server/level/ThreadedLevelLightEngine;Ljava/util/function/Function;Ljava/util/List;Lnet/minecraft/world/level/chunk/ChunkAccess;Z)Ljava/util/concurrent/CompletableFuture;"
             )
         )
-	public void overrideFlatWorld(Args args) {
+	public void overrideGeneratorType(Args args) {
 		ChunkAccess chunkAccess = args.get(8);
 
+        WorldPreset preset = BuiltinRegistries.WORLD_PRESET.get(WorldPresets.FLAT);
+        LevelStem levelStem = preset.overworldOrThrow();
+        ChunkGenerator generator = levelStem.generator();
+
         if (chunkAccess.getPos().getRegionX() < 0) {
-            logger.info("This is a negative chunk in the x axis");
+            args.set(3, generator);
         }
 	}
 }

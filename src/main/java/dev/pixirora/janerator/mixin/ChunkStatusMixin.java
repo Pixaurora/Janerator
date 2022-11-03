59c2748096ec623ba8189e95c544b9667b6ba1f5
Lilly Rose Berner <lilly@lostluma.dev>
2022-11-04 00:41:16 +0100

    Add customized default chunk generator (#1)

--- src/main/java/dev/pixirora/janerator/mixin/ChunkStatusMixin.java
+++ src/main/java/dev/pixirora/janerator/mixin/ChunkStatusMixin.java
@@ -1,34 +1,31 @@
 package dev.pixirora.janerator.mixin;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import net.minecraft.world.level.chunk.ChunkStatus;
-import net.minecraft.world.level.dimension.LevelStem;
-import net.minecraft.world.level.levelgen.presets.WorldPreset;
-import net.minecraft.world.level.levelgen.presets.WorldPresets;
+import net.minecraft.world.level.levelgen.FlatLevelSource;
+import net.minecraft.world.level.levelgen.flat.FlatLayerInfo;
+import net.minecraft.world.level.levelgen.flat.FlatLevelGeneratorSettings;
+import net.minecraft.world.level.levelgen.structure.StructureSet;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
-// import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
+import net.minecraft.core.HolderSet;
 import net.minecraft.data.BuiltinRegistries;
-// import net.minecraft.server.level.ServerLevel;
-// import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
-// import net.minecraft.server.level.ThreadedLevelLightEngine;
-// import java.util.function.Function;
-// import com.mojang.datafixers.util.Either;
-// import java.util.concurrent.Executor;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.chunk.ChunkAccess;
-// import net.minecraft.server.level.ChunkHolder;
-// import java.util.concurrent.CompletableFuture;
-// import java.util.List;
-import net.minecraft.world.level.chunk.ChunkGenerator;
 
 @Mixin(ChunkStatus.class)
 public class ChunkStatusMixin {
-    private static final Logger logger = LoggerFactory.getLogger("ChunkStatusMixin");
+    private static final FlatLevelSource generator = defaultSource();
 
 	@ModifyArgs(
         method = "generate(Ljava/util/concurrent/Executor;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplateManager;Lnet/minecraft/server/level/ThreadedLevelLightEngine;Ljava/util/function/Function;Ljava/util/List;Z)Ljava/util/concurrent/CompletableFuture;", 
@@ -40,12 +37,27 @@ public class ChunkStatusMixin {
 	public void overrideGeneratorType(Args args) {
 		ChunkAccess chunkAccess = args.get(8);
 
-        WorldPreset preset = BuiltinRegistries.WORLD_PRESET.get(WorldPresets.FLAT);
-        LevelStem levelStem = preset.overworldOrThrow();
-        ChunkGenerator generator = levelStem.generator();
-
         if (chunkAccess.getPos().getRegionX() < 0) {
             args.set(3, generator);
         }
 	}
+
+    private static FlatLevelSource defaultSource() {
+        List<FlatLayerInfo> layers = new ArrayList<>();
+
+        layers.add(new FlatLayerInfo(1, Blocks.BEDROCK));
+        layers.add(new FlatLayerInfo(63, Blocks.DEEPSLATE));
+
+        layers.add(new FlatLayerInfo(60, Blocks.STONE));
+        layers.add(new FlatLayerInfo(2, Blocks.DIRT));
+        layers.add(new FlatLayerInfo(1, Blocks.GRASS_BLOCK));
+
+        Optional<HolderSet<StructureSet>> optional = Optional.empty();
+        FlatLevelGeneratorSettings settings = new FlatLevelGeneratorSettings(optional, BuiltinRegistries.BIOME).withLayers(layers, optional);
+
+        ResourceKey<Biome> biomeResourceKey = Biomes.MUSHROOM_FIELDS;
+        settings.setBiome(BuiltinRegistries.BIOME.getOrCreateHolderOrThrow(biomeResourceKey));
+
+        return new FlatLevelSource(BuiltinRegistries.STRUCTURE_SETS, settings);
+    }
 }

59b50adb1d1cc2f93c79b4fb99822d6eeec7a2d9
Pixirora <rina@pixirora.dev>
2022-11-04 23:16:36 -0400

    Add different flat generators for end and nether

--- src/main/java/dev/pixirora/janerator/mixin/ChunkStatusMixin.java
+++ src/main/java/dev/pixirora/janerator/mixin/ChunkStatusMixin.java
@@ -18,6 +18,8 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 import net.minecraft.core.HolderSet;
 import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.block.Blocks;
@@ -25,7 +27,9 @@ import net.minecraft.world.level.chunk.ChunkAccess;
 
 @Mixin(ChunkStatus.class)
 public class ChunkStatusMixin {
-    private static final FlatLevelSource generator = defaultSource();
+    private static final FlatLevelSource generator = defaultSource(overworldLayers());
+    private static final FlatLevelSource netherGenerator = defaultSource(netherLayers());
+    private static final FlatLevelSource endGenerator = defaultSource(endLayers());
 
 	@ModifyArgs(
         method = "generate(Ljava/util/concurrent/Executor;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplateManager;Lnet/minecraft/server/level/ThreadedLevelLightEngine;Ljava/util/function/Function;Ljava/util/List;Z)Ljava/util/concurrent/CompletableFuture;", 
@@ -36,13 +40,22 @@ public class ChunkStatusMixin {
         )
 	public void overrideGeneratorType(Args args) {
 		ChunkAccess chunkAccess = args.get(8);
+        ServerLevel serverLevel = args.get(2);
 
         if (chunkAccess.getPos().getRegionX() >= 0) {
-            args.set(3, generator);
+            ResourceKey<Level> dimension = serverLevel.dimension();
+
+            if (dimension == Level.OVERWORLD) {
+                args.set(3, generator);
+            } else if (dimension == Level.NETHER) {
+                args.set(3, netherGenerator);
+            } else if (dimension == Level.END) {
+                args.set(3, endGenerator);
+            }
         }
 	}
 
-    private static FlatLevelSource defaultSource() {
+    private static List<FlatLayerInfo> overworldLayers() {
         List<FlatLayerInfo> layers = new ArrayList<>();
 
         layers.add(new FlatLayerInfo(1, Blocks.BEDROCK));
@@ -52,6 +65,32 @@ public class ChunkStatusMixin {
         layers.add(new FlatLayerInfo(2, Blocks.DIRT));
         layers.add(new FlatLayerInfo(1, Blocks.GRASS_BLOCK));
 
+        return layers;
+    }
+
+    private static List<FlatLayerInfo> netherLayers() {
+        List<FlatLayerInfo> layers = new ArrayList<>();
+
+        layers.add(new FlatLayerInfo(1, Blocks.BEDROCK));
+        layers.add(new FlatLayerInfo(30, Blocks.NETHERRACK));
+        layers.add(new FlatLayerInfo(1, Blocks.WARPED_NYLIUM));
+
+        return layers;
+    }
+
+    private static List<FlatLayerInfo> endLayers() {
+        List<FlatLayerInfo> layers = new ArrayList<>();
+
+        layers.add(new FlatLayerInfo(1, Blocks.BEDROCK));
+
+        layers.add(new FlatLayerInfo(59, Blocks.STONE));
+        layers.add(new FlatLayerInfo(2, Blocks.DIRT));
+        layers.add(new FlatLayerInfo(1, Blocks.GRASS_BLOCK));
+
+        return layers;
+    }
+
+    private static FlatLevelSource defaultSource(List<FlatLayerInfo> layers) {
         Optional<HolderSet<StructureSet>> optional = Optional.empty();
         FlatLevelGeneratorSettings settings = new FlatLevelGeneratorSettings(optional, BuiltinRegistries.BIOME).withLayers(layers, optional);
 

01b960a4ea3d9a126aff81009cacafe38dc40dae
Pixirora <rina@pixirora.dev>
2023-02-11 01:14:07 -0500

    Prevent prod-only crash

--- src/main/java/dev/pixirora/janerator/mixin/ChunkGeneratorMixin.java
+++ src/main/java/dev/pixirora/janerator/mixin/ChunkGeneratorMixin.java
@@ -6,8 +6,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import dev.pixirora.janerator.Janerator;
+import dev.pixirora.janerator.worldgen.JaneratorGenerator;
 import dev.pixirora.janerator.worldgen.MultiGenerator;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.StructureManager;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.ChunkGenerator;
@@ -15,7 +18,9 @@ import net.minecraft.world.level.chunk.ChunkGeneratorStructureState;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 
 @Mixin(ChunkGenerator.class)
-public class ChunkGeneratorMixin {
+public class ChunkGeneratorMixin implements JaneratorGenerator {
+    public void janerator$setDimension(ResourceKey<Level> dimension) {}
+
     @Inject(
         method = "createStructures(Lnet/minecraft/core/RegistryAccess;Lnet/minecraft/world/level/chunk/ChunkGeneratorStructureState;Lnet/minecraft/world/level/StructureManager;Lnet/minecraft/world/level/chunk/ChunkAccess;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplateManager;)V",
         at = @At("HEAD"),

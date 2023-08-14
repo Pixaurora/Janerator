0ef253f566e1ea599790e3ab50bafc153cbe12ee
Pixirora <rina@pixirora.dev>
2023-08-14 16:38:33 -0400

    switch to using generic codec for generators instead

--- src/main/java/net/pixaurora/janerator/config/GraphProperties.java
+++ src/main/java/net/pixaurora/janerator/config/GraphProperties.java
@@ -6,15 +6,15 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.levelgen.FlatLevelSource;
+import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.pixaurora.janerator.graphing.Grapher;
 import net.pixaurora.janerator.graphing.ConfiguredGrapherSettings;
 
 public class GraphProperties {
     private ResourceKey<Level> dimension;
     private ConfiguredGrapherSettings grapherSettings;
-    private FlatLevelSource shadedGenerator;
-    private FlatLevelSource outlineGenerator;
+    private ChunkGenerator shadedGenerator;
+    private ChunkGenerator outlineGenerator;
 
     private ThreadLocal<Grapher> grapher;
 
@@ -22,12 +22,12 @@ public class GraphProperties {
         instance -> instance.group(
             ResourceKey.codec(Registries.DIMENSION).fieldOf("dimension").forGetter(GraphProperties::getDimension),
             ConfiguredGrapherSettings.CODEC.fieldOf("function_to_graph").forGetter(GraphProperties::getGrapherSettings),
-            FlatLevelSource.CODEC.fieldOf("shaded_in_generator").forGetter(GraphProperties::getShadedGenerator),
-            FlatLevelSource.CODEC.fieldOf("outlines_generator").forGetter(GraphProperties::getOutlineGenerator)
+            ChunkGenerator.CODEC.fieldOf("shaded_in_generator").forGetter(GraphProperties::getShadedGenerator),
+            ChunkGenerator.CODEC.fieldOf("outlines_generator").forGetter(GraphProperties::getOutlineGenerator)
         ).apply(instance, GraphProperties::new)
     );
 
-    public GraphProperties(ResourceKey<Level> dimension, ConfiguredGrapherSettings grapherSettings, FlatLevelSource shadedGenerator, FlatLevelSource outlineGenerator) {
+    public GraphProperties(ResourceKey<Level> dimension, ConfiguredGrapherSettings grapherSettings, ChunkGenerator shadedGenerator, ChunkGenerator outlineGenerator) {
         this.dimension = dimension;
 
         this.grapherSettings = grapherSettings;
@@ -50,11 +50,11 @@ public class GraphProperties {
         return this.grapher.get();
     }
 
-    public FlatLevelSource getShadedGenerator() {
+    public ChunkGenerator getShadedGenerator() {
         return this.shadedGenerator;
     }
 
-    public FlatLevelSource getOutlineGenerator() {
+    public ChunkGenerator getOutlineGenerator() {
         return this.outlineGenerator;
     }
 }

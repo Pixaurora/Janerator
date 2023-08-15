cc4ed853e5c0b59f3487602741ce755528bd0284
Pixirora <rina@pixirora.dev>
2023-08-15 18:49:03 -0400

    cache graphed chunks to reduce repeated calculations

--- src/main/java/net/pixaurora/janerator/graphing/ChunkGrapher.java
+++ src/main/java/net/pixaurora/janerator/graphing/ChunkGrapher.java
@@ -1,17 +1,30 @@
 package net.pixaurora.janerator.graphing;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.ChunkPos;
 
-public class ChunkGrapher {
+public class ChunkGrapher extends CacheLoader<ChunkPos, GraphedChunk> {
     private ConfiguredGrapherSettings settings;
     private ThreadLocal<PointGrapher> localPointGrapher;
+
+    private LoadingCache<ChunkPos, GraphedChunk> cache;
 
     public ChunkGrapher(ConfiguredGrapherSettings settings) {
         this.settings = settings;
         this.localPointGrapher = ThreadLocal.withInitial(() -> PointGrapher.fromConfig(this.settings));
+
+        this.cache = CacheBuilder.newBuilder()
+            .expireAfterWrite(60, TimeUnit.SECONDS)
+            .maximumSize(1024)
+            .build(this);
     }
 
     public ConfiguredGrapherSettings getSettings() {
@@ -30,8 +43,17 @@ public class ChunkGrapher {
         return CompletableFuture.supplyAsync(() -> this.isPointShaded(x, z), GraphingUtils.threadPool);
     }
 
-    public GraphedChunk getChunkGraph(ChunkPos pos) {
+    @Override
+    public GraphedChunk load(ChunkPos pos) {
         return new GraphedChunk(this, pos);
+    }
+
+    public GraphedChunk getChunkGraph(ChunkPos pos) {
+        try {
+            return this.cache.get(pos);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public CompletableFuture<GraphedChunk> scheduleChunkGraphing(ChunkPos pos) {

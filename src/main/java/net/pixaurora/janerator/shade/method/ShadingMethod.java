6a28d9a26d9f019c9af0b755829f30b5c17a86b4
Pixirora <rina@pixirora.dev>
2023-10-15 20:16:02 -0400

    use either codec to make single-generator shading require less input

--- src/main/java/net/pixaurora/janerator/shade/method/ShadingMethod.java
+++ src/main/java/net/pixaurora/janerator/shade/method/ShadingMethod.java
@@ -2,7 +2,9 @@ package net.pixaurora.janerator.shade.method;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
+import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 
 import net.minecraft.world.level.chunk.ChunkGenerator;
@@ -12,7 +14,20 @@ import net.pixaurora.janerator.graphing.Coordinate;
 
 public interface ShadingMethod extends SpecifiesType<ShadingMethod> {
     public static final List<SerialType<ShadingMethod>> TYPES = new ArrayList<>(List.of(NormalShading.TYPE));
-    public static final Codec<ShadingMethod> CODEC = new SerialType.Group<>("Shading method", TYPES).dispatchCodec();
+    public static final Codec<ShadingMethod> BASE_CODEC = new SerialType.Group<>("Shading method", TYPES).dispatchCodec();
+    public static final Codec<ShadingMethod> CODEC = Codec.either(
+        ChunkGenerator.CODEC,
+        ShadingMethod.BASE_CODEC
+    ).xmap(
+        either -> either.map(NormalShading::new, Function.identity()),
+        shading -> {
+            if (shading instanceof NormalShading basicShading) {
+                return Either.left(basicShading.generator());
+            } else {
+                return Either.right(shading);
+            }
+        }
+    );
 
     public List<ShadeData> shadeIn(List<Coordinate> points);
 
